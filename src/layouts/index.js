faa44264fcc198b581224ec34344c0524c246b5b
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2018-06-11 15:07:27 +0200

    Add webfont and normalize.css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { canUseDOM } from 'exenv';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import 'normalize.css';
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
-    <div>{children()}</div>
-  </div>
-)
+import Navbar from '../components/Navbar';
+
+import './index.sass';
+
+class TemplateWrapper extends Component {
+  componentWillMount() {
+    if (canUseDOM) {
+      const WebFont = require('webfontloader');
+      WebFont.load({
+        google: {
+          families: ['Open Sans']
+        }
+      });
+    }
+  }
+    
+  render() {
+    const { children } = this.props;
+    return (
+      <div className='site-container'>
+        <Helmet title='Debout pour les Enfants' />
+        <Navbar />
+        {children()}
+      </div>
+    );
+  }
+}
+
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
