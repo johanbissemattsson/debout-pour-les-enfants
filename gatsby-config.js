3b711443e3ecef2a7a8fb0065632f2a79c37c3da
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2018-06-20 14:29:18 +0200

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,24 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-catch-links',
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },    
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
